bfbd14b63af01616ca8f184cdc09318c7c51bc72
dennis <d.wruck@brillen.de>
2018-04-25 01:12:33 +0530

    Testing 2

--- demo.js
+++ demo.js
@@ -4,11 +4,11 @@ module.exports = {
 	console.log(phantom);
 	var page = phantom.create();
 	//console.log(page);
-	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-    page.evaluate(function() {
-      console.log("$");
-    });
-    phantom.exit()});
+	// page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+ //    page.evaluate(function() {
+ //      console.log("$");
+ //    });
+ //    phantom.exit()});
 	//console.log($);
 	var uri_enc = encodeURIComponent(data);
     console.log(uri_enc);
